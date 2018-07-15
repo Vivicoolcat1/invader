449f599905eed656993f167e5a0cbfcb2eaff95a
Vivicoolcat1 <Vivicoolcat1@users.noreply.github.com>
2018-07-15 11:30:35 -0700

    Update LeagueInvaders.java

--- src/invader/LeagueInvaders.java
+++ src/invader/LeagueInvaders.java
@@ -1,5 +1,25 @@
 package invader;
 
-public class LeagueInvaders {
+import javax.swing.JFrame;
 
+public class LeagueInvaders {
+	JFrame frame ;
+	LeagueInvaders a = new LeagueInvaders();
+	int width = 500;
+	int height = 800;
+public  static void main(String[] args) {
+	setup(a);
+}
+public void LeaugeInvaders() {
+	frame=new JFrame();
+	
+	
+}
+void setup() {
+	
+	frame.setSize(width, height);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+}
 }
